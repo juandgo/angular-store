bcd5b28e1191c929be68317c13db8b947c8b06ea
juandgo <gojuanda01@gmail.com>
2025-04-17 17:42:59 -0500

    info product implemented

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -3,9 +3,10 @@ import { Product } from '@shared/models/product.model';
 import { CommonModule } from '@angular/common';
 import { ReversePipe } from '@shared/pipes/reverse.pipe';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';  
+import { RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'app-product',
-  imports: [CommonModule, ReversePipe, TimeAgoPipe],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe, RouterLinkWithHref],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
